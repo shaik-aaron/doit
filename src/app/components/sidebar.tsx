d784659adf5496966decb7c02a13e6b4dbaee10c
shaik-aaron <shaikaaronbbhs@gmail.com>
2024-07-03 23:48:24 +0530

    dummy feat to add boards

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,11 +1,38 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
-import { EyeOff, MoonStar, Sun } from "lucide-react";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { EyeOff, MoonStar, Sun, Table, X } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function SideBar() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+  const [columns, setColumns] = useState<any>([
+    {
+      id: 0,
+      name: "To do",
+    },
+    {
+      id: 1,
+      name: "Doing",
+    },
+  ]);
+  const [boardName, setBoardName] = useState<string>("");
+  const [boards, setBoards] = useState<string[]>([]);
+
+  function handleChange(index: number, val: string) {
+    const temp = columns.map((column: any, i: number) => {
+      if (i === index) {
+        return { ...column, name: val };
+      } else {
+        return column;
+      }
+    });
+    setColumns(temp);
+  }
 
   useEffect(() => {
     if (isDarkMode) {
@@ -16,7 +43,7 @@ export default function SideBar() {
   }, [isDarkMode]);
 
   return (
-    <div className="pt-7 pr-6 w-[300px] flex flex-col justify-between bg-white h-full border-r border-[#E4EBFA] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] dark:bg-dark-grey dark:border-lines-dark">
+    <div className="pt-7 pr-6 min-w-[300px] flex flex-col justify-between bg-white h-full border-r border-[#E4EBFA] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] dark:bg-dark-grey dark:border-lines-dark">
       <div>
         <div className="pl-7 flex items-center gap-4">
           <Image src={"/logo/logo.png"} alt="Logo" width={24} height={24} />
@@ -24,10 +51,76 @@ export default function SideBar() {
             doit.
           </p>
         </div>
-        <div className="mt-14 tracking-[2.4px]">
-          <p className="pl-7 font-primary text-md-grey font-bold">
+        <div className="mt-14">
+          <p className="pl-7 tracking-[2.4px] mb-5 font-primary text-md-grey font-bold">
             ALL BOARDS (3)
           </p>
+          {boards.map((board: string) => {
+            return (
+              <div className="flex gap-4 py-4 pl-7 rounded-r-full cursor-pointer text-md-grey hover:text-white hover:bg-main-purple">
+                <Table />
+                <p className="text-base dark:text-base font-primary font-bold">
+                  {board}
+                </p>
+              </div>
+            );
+          })}
+          <Dialog>
+            <DialogTrigger className="w-full">
+              <div className="flex gap-4 py-4 pl-7 rounded-r-full cursor-pointer text-main-purple hover:text-white hover:bg-main-purple">
+                <Table />
+                <p className="text-base dark:text-base font-primary font-bold">
+                  + Create New Board
+                </p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <p className="dark:text-white font-primary text-lg font-bold ">
+                Add New Board
+              </p>
+              <p className="dark:text-white text-xs font-primary">Board Name</p>
+              <Input
+                onChange={(e) => setBoardName(e.target.value)}
+                className="border border-md-grey"
+                placeholder="e.g. Web Design"
+              />
+              <p className="dark:text-white text-xs font-primary">
+                Board Columns
+              </p>
+              {columns.map((column: any, i: number) => {
+                return (
+                  <div className="flex w-full items-center gap-4">
+                    <Input
+                      className="border border-md-grey"
+                      onChange={(e) => handleChange(i, e.target.value)}
+                      value={column.name}
+                    />
+                    <X
+                      onClick={() =>
+                        setColumns((prev: any) =>
+                          prev.filter((_: any, index: number) => i !== index)
+                        )
+                      }
+                      className="cursor-pointer"
+                      color="#828FA3"
+                    />
+                  </div>
+                );
+              })}
+              <Button
+                onClick={(e) => setColumns((prev: any) => [...prev, ""])}
+                className="rounded-2xl hover:bg-white bg-white text-main-purple text-xs font-bold font-primary"
+              >
+                + Add New Column
+              </Button>
+              <Button
+                onClick={() => setBoards((prev) => [...prev, boardName])}
+                className="rounded-2xl hover:bg-main-purple bg-main-purple text-white text-xs font-bold font-primary"
+              >
+                Create New Board
+              </Button>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div>
